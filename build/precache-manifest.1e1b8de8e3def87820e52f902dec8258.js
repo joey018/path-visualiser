6551beff4fd0f02f0448eed4c4c473ecc1cc964c
joey018 <92970006+joey018@users.noreply.github.com>
2021-10-22 11:20:51 +0530

    Add files via upload

---
+++ build/precache-manifest.1e1b8de8e3def87820e52f902dec8258.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7a0210ae5aa7b4f643cdc4c48a8fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df4f5074ee1b29ce92de",
+    "url": "/static/css/main.63b4ad43.chunk.css"
+  },
+  {
+    "revision": "1607134a6293a38ea195",
+    "url": "/static/js/2.b2180c49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2180c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4f5074ee1b29ce92de",
+    "url": "/static/js/main.79aa19d9.chunk.js"
+  },
+  {
+    "revision": "749e497a425ebe12b7ac",
+    "url": "/static/js/runtime-main.916aef06.js"
+  },
+  {
+    "revision": "579f199d1b9fd1edc027c3730800b342",
+    "url": "/static/media/path.579f199d.png"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/static/media/play-solid.f79fe6ae.svg"
+  },
+  {
+    "revision": "bb7daa97874804191be47e5cd858409e",
+    "url": "/static/media/times-circle-solid.bb7daa97.svg"
+  }
+]);
